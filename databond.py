50ddf5c410d5d79647d4bd45bc7c7eb0f0ef010a
Rob Salmond <rob@salmond.ca>
2016-11-23 06:23:00 +0000

    use a sqlalchemy class registry to find table mapper objects, and sort tables to be inserted so that foreign key constraints can be satisfied

--- databond.py
+++ databond.py
@@ -3,14 +3,11 @@ import sys
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative.api import DeclarativeMeta
 
-#Create and engine and get the metadata
-
 classfile = 'classes.py'
 dbconfigfile = 'db.py'
 
 def create_orm_classes():
     """ create a python file with sqlalchemy class for each table in the db """
-    classes = []
     with open(classfile, 'w') as f:
         f.write('from db import Base, source_metadata\n')
         f.write('from sqlalchemy import Table\n')
@@ -18,19 +15,18 @@ def create_orm_classes():
             if table == 'sqlite_sequence':
                 continue
             classname = table.lower().capitalize()
-            classes.append(classname)
             f.write('class {}(Base):\n'.format(classname))
             f.write('\t__table__ = Table(\'{}\', source_metadata, autoload=True)\n'.format(table))
 
-    return classes
-
 def create_db_config(args):
+    """ create a python file with db config and sqlalchemy init """
     db_name = args.sqlite_file.split('.')[0]
     with open(dbconfigfile, 'w') as f:
         f.write('from sqlalchemy import * \n')
         f.write('from sqlalchemy.orm import create_session\n')
         f.write('from sqlalchemy.ext.declarative import declarative_base\n')
-        f.write('Base = declarative_base()\n')
+        f.write('classreg = {}\n')
+        f.write('Base = declarative_base(class_registry=classreg)\n')
         f.write('source_engine = create_engine(\'sqlite:///{}\')\n'.format(args.sqlite_file))
         f.write('source_metadata = MetaData(bind=source_engine)\n')
         f.write('source_metadata.reflect(source_engine)\n')
@@ -56,6 +52,41 @@ def create_mysql_db(args):
     result = conn.execute('create database {}'.format(db_name))
     conn.close()
 
+
+def sort_mappers(classes):
+    """ try to put mapper objects / tables into an insert order which will allow foreign key constraints to be satisfied """
+    order = []
+
+    #iterate over a copy of the list
+    classlist = dict(classes)
+
+    #not sure what this is, probably dont need it?
+    del classlist['_sa_module_registry']
+
+    # because tables with foreign key constraints may come up in the list prior to the tables they rely on
+    # we may skip some tables one (or more) times and need to re-iterate the list until we have them all
+    #
+    #XXX: circular dependencies could put this into an endless loop?!
+    #
+    while True:
+        for name, cls in classlist.items():
+            # tables with no foreign key constraints can be inserted into the new db at any time
+            if len(cls.__table__.foreign_key_constraints) == 0:
+                #lower case it for later lookup by table name
+                order.append(name.lower())
+                del classlist[name]
+            else:
+                foreign_tables = [fkc.referred_table.name for fkc in cls.__table__.foreign_key_constraints]
+                # if all tables with foreign keys are ahead of this one its safe to add it to the queue
+                if set(foreign_tables).issubset(set(order)):
+                    order.append(name.lower())
+                    del classlist[name]
+
+        if len(classlist.items()) == 0:
+            break
+
+    return order
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="SQLite database exporter.")
@@ -78,7 +109,7 @@ if __name__ == '__main__':
     print('Creating target database.')
     create_mysql_db(args)
 
-    classes = create_orm_classes()
+    create_orm_classes()
 
     from classes import *
 
@@ -89,16 +120,14 @@ if __name__ == '__main__':
     print('Creating tables in target database.')
     source_metadata.create_all(bind=target_engine)
 
+    sorted_mappers = sort_mappers(classreg)
 
-    for var in dict(locals()):
-        # XXX: I *think* this is safe ...
-        if locals().get(var).__class__ is DeclarativeMeta:
-            baseclass = locals().get(var)
-            if hasattr(baseclass, '__mapper__'):
-                print('Importing data for {}.'.format(baseclass))
-                for row in source_session.query(baseclass).all():
-                    print('Inserting row {}'.format(row.id))
-                    #source_session.expunge(row)
-                    target_session.merge(row)
-                    target_session.flush()
+    for mappername in sorted_mappers:
+        mapperobj = classreg[mappername.capitalize()]
+        print('Importing data for table {}.'.format(mappername))
+        for row in source_session.query(mapperobj).all():
+            print('Inserting row {}'.format(row.id))
+            #source_session.expunge(row)
+            target_session.merge(row)
+            target_session.flush()
 
